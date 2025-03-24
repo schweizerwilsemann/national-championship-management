debb1f135d93ebb31e8dfc79799223250b6d9525
project_Html <khoanguyenduc99@gmail.com>
2025-03-24 11:41:45 +0700

    customize axios for 2 domain

--- client/src/utilities/customize/axios.customize.ts
+++ client/src/utilities/customize/axios.customize.ts
@@ -1,43 +1,56 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+import { getCookie } from "cookies-next";
 
 const backendURL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1`;
-const instance = axios.create({
+const socialBackendURL = `${process.env.NEXT_PUBLIC_SOCIAL_API_URL}/api`; // API Express
+
+const instance: AxiosInstance = axios.create({
   baseURL: backendURL,
   withCredentials: true,
 });
-instance.interceptors.request.use(
-  async function (config) {
-    const accessToken = document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("access_token="))
-      ?.split("=")[1];
 
-    if (accessToken) {
-      config.headers["Authorization"] = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
-);
-instance.interceptors.response.use(
-  function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    if (response.data) {
-      return response.data;
-    }
-    return response;
-  },
-  function (error) {
-    if (error?.response?.data) {
-      return error?.response?.data;
-    }
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
-  }
-);
+const socialInstance: AxiosInstance = axios.create({
+  baseURL: socialBackendURL,
+  withCredentials: true,
+});
 
-export default instance;
+const setupInterceptors = (instance: AxiosInstance, tokenKey: string) => {
+  instance.interceptors.request.use(
+    async function (config) {
+      let accessToken: string | undefined;
+
+      if (typeof window !== "undefined") {
+        // Ép kiểu về string hoặc undefined
+        accessToken = getCookie(tokenKey) as string | undefined;
+      }
+
+      if (accessToken) {
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+      }
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  instance.interceptors.response.use(
+    function (response) {
+      if (response.data) {
+        return response.data;
+      }
+      return response;
+    },
+    function (error) {
+      if (error?.response?.data) {
+        return error?.response?.data;
+      }
+      return Promise.reject(error);
+    }
+  );
+};
+
+setupInterceptors(instance, "access_token");
+setupInterceptors(socialInstance, "access_token_social");
+
+export { instance, socialInstance };
