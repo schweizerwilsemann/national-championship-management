c4ca04fc9c996df3572c15232f54489f92f5749d
project_Html <khoanguyenduc99@gmail.com>
2025-03-24 12:40:25 +0700

    add meta

---
+++ client/src/components/other/meta.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Meta = () => {
+    return (
+        <Head>
+            <title>Premier League - Official Site</title>
+            <meta name="description" content="Premier League Official Website" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    );
+};
+
+export default Meta;
